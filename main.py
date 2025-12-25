d39185f4ee33294059534df2d79dfd87fbf518aa
BFiato <BradFiato@gmail.com>
2025-12-25 00:05:38 -0500

    J: added object containers and cleaned up code in main.py

--- main.py
+++ main.py
@@ -3,54 +3,39 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
 
-
-
 def main():
     pygame.init()
-    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock()
     
-    pygame.time.Clock
-    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     dt = 0 
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-            
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
+    log_state()
     
     while True:
-        
         log_state()
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
         
         screen.fill("black")
 
-        
-        player.draw(screen)
-    
-        player.update(dt)
-        
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
         
         dt = pygame.time.Clock().tick(60) / 1000
-        
-        print(dt)
-    
-
-
-    #print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
-    #if __name__ == "__main__":
-
-        
-
 
 if __name__ == "__main__":
-    
 
     main()
